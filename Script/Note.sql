42d51082b2aa305c26764f0cebfd08e2b14e226c
heangphanet <phanet.heang@sunfixconsulting.com>
2025-02-27 08:49:15 +0700

    Add script to create table

---
+++ Script/Note.sql
@@ -0,0 +1,15 @@
+USE [NOTE]
+GO
+
+
+
+CREATE TABLE [dbo].[Notes](
+	[Id] [int] IDENTITY(1,1) NOT NULL Primary Key,
+	[UserID] [int] NOT NULL,
+	[Title] [nvarchar](50) NOT NULL,
+	[Content] [nvarchar](100) NULL,
+	[CreatedAt] [nvarchar](20) NOT NULL,
+	[UpdatedAt] [nvarchar](20) NULL,
+)
+
+
